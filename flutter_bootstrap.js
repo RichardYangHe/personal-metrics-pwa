02964f1f881bfae554a1a5702e5717222d2393af
RichardYangHe <yvorqqap@gmail.com>
2026-07-05 10:49:19 +0800

    Deploy metrics PWA V0.24.5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2193016069" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "942171366" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
